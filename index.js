ce34e9eb1a274db081af8ab2f0f9d613ba796d35
med <mohammed.bennaceur98@gmail.com>
2023-08-29 13:53:28 +0100

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const writeCards = (arr, msg) =>
+  arr.map((person) => `Thank you, ${person}, for the wonderful ${msg} gift!`);
+
+const countDown = (number) => {
+  let count = number;
+  while (count >= 0) {
+    console.log(count);
+    count--;
+  }
+};
